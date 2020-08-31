2b032acd767a24a340669071dde462db61b0b18a
khaireny <khaireny@gmail.com>
2020-08-31 17:18:14 +0700

    my first commit to repository

---
+++ iGlossary/GlossaryList.swift
@@ -0,0 +1,21 @@
+//
+//  GlossaryList.swift
+//  iGlossary
+//
+//  Created by A Khairini on 29/08/20.
+//  Copyright © 2020 Infinite Learning ADA. All rights reserved.
+//
+
+import SwiftUI
+
+struct GlossaryList: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Glossary_Previews: PreviewProvider {
+    static var previews: some View {
+        GlossaryList()
+    }
+}
